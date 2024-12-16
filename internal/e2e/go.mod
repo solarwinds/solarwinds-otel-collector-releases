4a8606bb1e8a25b1da76bb5fa29c72ba5c2f0f66
Jiri Janecek <jiri.janecek@solarwinds.com>
2024-12-16 13:52:53 +0100

    Initial sketch for Metrics, Logs and traces integration tests.

--- internal/e2e/go.mod
+++ internal/e2e/go.mod
@@ -3,7 +3,9 @@ module github.com/solarwinds/solarwinds-otel-collector/internal/e2e
 go 1.23.4
 
 require (
-    github.com/testcontainers/testcontainers-go v0.34.0
+	github.com/stretchr/testify v1.10.0
+	github.com/testcontainers/testcontainers-go v0.34.0
+	go.opentelemetry.io/collector/pdata v1.21.0
 )
 
 require (
@@ -26,7 +28,9 @@ require (
 	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.17.4 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/moby/docker-image-spec v1.3.1 // indirect
@@ -34,6 +38,8 @@ require (
 	github.com/moby/sys/sequential v0.5.0 // indirect
 	github.com/moby/sys/user v0.1.0 // indirect
 	github.com/moby/term v0.5.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/morikuni/aec v1.0.0 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.1.0 // indirect
@@ -43,7 +49,6 @@ require (
 	github.com/shirou/gopsutil/v3 v3.23.12 // indirect
 	github.com/shoenig/go-m1cpu v0.1.6 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
-	github.com/stretchr/testify v1.9.0 // indirect
 	github.com/tklauser/go-sysconf v0.3.12 // indirect
 	github.com/tklauser/numcpus v0.6.1 // indirect
 	github.com/yusufpapurcu/wmi v1.2.3 // indirect
@@ -51,7 +56,14 @@ require (
 	go.opentelemetry.io/otel v1.24.0 // indirect
 	go.opentelemetry.io/otel/metric v1.24.0 // indirect
 	go.opentelemetry.io/otel/trace v1.24.0 // indirect
-	golang.org/x/crypto v0.22.0 // indirect
-	golang.org/x/sys v0.21.0 // indirect
+	go.opentelemetry.io/proto/otlp v1.4.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/crypto v0.29.0 // indirect
+	golang.org/x/net v0.31.0 // indirect
+	golang.org/x/sys v0.27.0 // indirect
+	golang.org/x/text v0.20.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20241118233622-e639e219e697 // indirect
+	google.golang.org/grpc v1.68.0 // indirect
+	google.golang.org/protobuf v1.35.2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/e2e/integration_test.go
+++ internal/e2e/integration_test.go
@@ -2,22 +2,33 @@ package e2e
 
 import (
 	"context"
-	"fmt"
+	"io"
 	"log"
-	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/pdata/pcommon"
+	"go.opentelemetry.io/collector/pdata/plog"
+	"go.opentelemetry.io/collector/pdata/pmetric"
+	"go.opentelemetry.io/collector/pdata/ptrace"
 
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/network"
 	"github.com/testcontainers/testcontainers-go/wait"
 )
 
-func TestSingleSolarWindsOtelCollector(t *testing.T) {
-	ctx, _ := context.WithCancel(context.Background())
+const (
+	receivingContainer  = "receiver"
+	testedContainer     = "sut"
+	generatingContainer = "generator"
+	port                = 17016
+)
+
+func TestMetricStream(t *testing.T) {
+	ctx := context.Background()
 
 	net, err := network.New(ctx)
 	require.NoError(t, err)
@@ -27,55 +38,210 @@ func TestSingleSolarWindsOtelCollector(t *testing.T) {
 	require.NoError(t, err)
 	testcontainers.CleanupContainer(t, rContainer)
 
-	eContainer, err := runEmittingSolarWindsOTELCollector(ctx, net.Name)
+	eContainer, err := runTestedSolarWindsOTELCollector(ctx, net.Name)
 	require.NoError(t, err)
 	testcontainers.CleanupContainer(t, eContainer)
 
-	<-time.After(20 * time.Second)
-	log.Println("***: termination in progress")
+	cmd := []string{
+		"metrics",
+		"--metrics", "10",
+		"--otlp-insecure",
+		"--otlp-endpoint", "sut:17016",
+		"--otlp-attributes", "resource.attributes.testing_attribute=\"testing_value\"",
+	}
 
-	err = eContainer.Terminate(ctx)
+	gContainer, err := runGeneratorContainer(ctx, net.Name, cmd)
+	require.NoError(t, err)
+	testcontainers.CleanupContainer(t, gContainer)
+
+	<-time.After(10 * time.Second)
+	log.Println("***: evaluation in progress")
+
+	expectedMetricsCount := 10
+	evaluateMetricsStream(t, ctx, rContainer, expectedMetricsCount)
+}
+
+func evaluateMetricsStream(
+	t *testing.T,
+	ctx context.Context,
+	container testcontainers.Container,
+	expectedCount int,
+) {
+	// Obtain result from container.
+	lines, err := loadResultFile(ctx, container, "/tmp/result.json")
 	require.NoError(t, err)
 
-	err = rContainer.Terminate(ctx)
+	ms := pmetric.NewMetrics()
+	jum := new(pmetric.JSONUnmarshaler)
+	for _, line := range lines {
+		m, err := jum.UnmarshalMetrics([]byte(line))
+		if err != nil {
+			continue
+		}
+
+		require.Equal(t, m.ResourceMetrics().Len(), 1, "it must contain exactly one resource metric")
+		evaluateResourceAttributes(t, m.ResourceMetrics().At(0).Resource().Attributes())
+		m.ResourceMetrics().MoveAndAppendTo(ms.ResourceMetrics())
+	}
+	require.Equal(t, ms.MetricCount(), expectedCount)
+}
+
+func TestTracesStream(t *testing.T) {
+	ctx := context.Background()
+
+	net, err := network.New(ctx)
 	require.NoError(t, err)
+	testcontainers.CleanupNetwork(t, net)
+
+	rContainer, err := runReceivingSolarWindsOTELCollector(ctx, net.Name)
+	require.NoError(t, err)
+	testcontainers.CleanupContainer(t, rContainer)
+
+	eContainer, err := runTestedSolarWindsOTELCollector(ctx, net.Name)
+	require.NoError(t, err)
+	testcontainers.CleanupContainer(t, eContainer)
+
+	cmd := []string{
+		"traces",
+		"--traces", "10",
+		"--otlp-insecure",
+		"--otlp-endpoint", "sut:17016",
+		"--otlp-attributes", "resource.attributes.testing_attribute=\"testing_value\"",
+	}
+
+	gContainer, err := runGeneratorContainer(ctx, net.Name, cmd)
+	require.NoError(t, err)
+	testcontainers.CleanupContainer(t, gContainer)
+
+	<-time.After(10 * time.Second)
+	log.Println("***: evaluation in progress")
+
+	expectedTracesCount := 10 * 2
+	evaluateTracesStream(t, ctx, rContainer, expectedTracesCount)
+}
+
+func evaluateTracesStream(
+	t *testing.T,
+	ctx context.Context,
+	container testcontainers.Container,
+	expectedCount int,
+) {
+	// Obtain result from container.
+	lines, err := loadResultFile(ctx, container, "/tmp/result.json")
+	require.NoError(t, err)
+
+	trs := ptrace.NewTraces()
+	jum := new(ptrace.JSONUnmarshaler)
+	for _, line := range lines {
+		tr, err := jum.UnmarshalTraces([]byte(line))
+		if err != nil {
+			continue
+		}
+
+		require.Equal(t, tr.ResourceSpans().Len(), 1, "it must contain exactly one resource span")
+		evaluateResourceAttributes(t, tr.ResourceSpans().At(0).Resource().Attributes())
+		tr.ResourceSpans().MoveAndAppendTo(trs.ResourceSpans())
+	}
+	require.Equal(t, expectedCount, trs.SpanCount())
+}
+
+func TestLogsStream(t *testing.T) {
+	ctx := context.Background()
+
+	net, err := network.New(ctx)
+	require.NoError(t, err)
+	testcontainers.CleanupNetwork(t, net)
+
+	rContainer, err := runReceivingSolarWindsOTELCollector(ctx, net.Name)
+	require.NoError(t, err)
+	testcontainers.CleanupContainer(t, rContainer)
+
+	eContainer, err := runTestedSolarWindsOTELCollector(ctx, net.Name)
+	require.NoError(t, err)
+	testcontainers.CleanupContainer(t, eContainer)
+
+	cmd := []string{
+		"logs",
+		"--logs", "10",
+		"--body", "testing log body",
+		"--otlp-insecure",
+		"--otlp-endpoint", "sut:17016",
+		"--otlp-attributes", "resource.attributes.testing_attribute=\"testing_value\"",
+	}
+
+	gContainer, err := runGeneratorContainer(ctx, net.Name, cmd)
+	require.NoError(t, err)
+	testcontainers.CleanupContainer(t, gContainer)
+
+	<-time.After(10 * time.Second)
+	log.Println("***: evaluation in progress")
+
+	expectedLogsCount := 10
+	evaluateLogsStream(t, ctx, rContainer, expectedLogsCount)
+}
+
+func evaluateLogsStream(
+	t *testing.T,
+	ctx context.Context,
+	container testcontainers.Container,
+	expectedCount int,
+) {
+	// Obtain result from container.
+	lines, err := loadResultFile(ctx, container, "/tmp/result.json")
+	require.NoError(t, err)
+
+	lgs := plog.NewLogs()
+	jum := new(plog.JSONUnmarshaler)
+	for _, line := range lines {
+		lg, err := jum.UnmarshalLogs([]byte(line))
+		if err != nil {
+			continue
+		}
+
+		require.Equal(t, lg.ResourceLogs().Len(), 1, "it must contain exactly one resource log")
+		evaluateResourceAttributes(t, lg.ResourceLogs().At(0).Resource().Attributes())
+		lg.ResourceLogs().MoveAndAppendTo(lgs.ResourceLogs())
+	}
+	require.Equal(t, expectedCount, lgs.LogRecordCount())
+}
+
+func evaluateResourceAttributes(
+	t *testing.T,
+	atts pcommon.Map,
+) {
+	val, ok := atts.Get("resource.attributes.testing_attribute")
+	require.True(t, ok, "testing attribute must exist")
+	require.Equal(t, val.AsString(), "testing_value", "testing attribute value must be the same")
 }
 
 func runReceivingSolarWindsOTELCollector(
 	ctx context.Context,
 	networkName string,
 ) (testcontainers.Container, error) {
+	containerName := receivingContainer
+
 	configPath, err := filepath.Abs(filepath.Join(".", "testdata", "receiving_collector.yaml"))
 	if err != nil {
 		return nil, err
 	}
 
-	r, err := os.Open(configPath)
-	if err != nil {
-		return nil, err
-	}
-
 	lc := new(MyLogConsumer)
-	lc.Prefix = "receiving"
+	lc.Prefix = containerName
 	req := testcontainers.ContainerRequest{
 		Image: "solarwinds-otel-collector:latest",
 		LogConsumerCfg: &testcontainers.LogConsumerConfig{
-			/*Opts: []testcontainers.LogProductionOption{
-				testcontainers.WithLogProductionTimeout(10 * time.Second),
-			},*/
 			Consumers: []testcontainers.LogConsumer{lc},
 		},
 		Files: []testcontainers.ContainerFile{
 			{
 				HostFilePath:      configPath,
-				Reader:            r,
 				ContainerFilePath: "/opt/default-config.yaml",
 				FileMode:          0o440,
 			},
 		},
 		WaitingFor: wait.ForLog("Everything is ready. Begin running and processing data."),
 		Networks:   []string{networkName},
-		Name:       "receiving",
+		Name:       containerName,
 	}
 
 	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
@@ -86,41 +252,34 @@ func runReceivingSolarWindsOTELCollector(
 	return container, err
 }
 
-func runEmittingSolarWindsOTELCollector(
+func runTestedSolarWindsOTELCollector(
 	ctx context.Context,
 	networkName string,
 ) (testcontainers.Container, error) {
+	containerName := testedContainer
+
 	configPath, err := filepath.Abs(filepath.Join(".", "testdata", "emitting_collector.yaml"))
 	if err != nil {
 		return nil, err
 	}
 
-	r, err := os.Open(configPath)
-	if err != nil {
-		return nil, err
-	}
-
 	lc := new(MyLogConsumer)
-	lc.Prefix = "emitting"
+	lc.Prefix = containerName
 	req := testcontainers.ContainerRequest{
 		Image: "solarwinds-otel-collector:latest",
 		LogConsumerCfg: &testcontainers.LogConsumerConfig{
-			/*Opts: []testcontainers.LogProductionOption{
-				testcontainers.WithLogProductionTimeout(10 * time.Second),
-			},*/
 			Consumers: []testcontainers.LogConsumer{lc},
 		},
 		Files: []testcontainers.ContainerFile{
 			{
 				HostFilePath:      configPath,
-				Reader:            r,
 				ContainerFilePath: "/opt/default-config.yaml",
 				FileMode:          0o440,
 			},
 		},
 		WaitingFor: wait.ForLog("Everything is ready. Begin running and processing data."),
 		Networks:   []string{networkName},
-		Name:       "emitting",
+		Name:       containerName,
 	}
 
 	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
@@ -129,6 +288,54 @@ func runEmittingSolarWindsOTELCollector(
 	})
 
 	return container, err
+}
+
+func runGeneratorContainer(
+	ctx context.Context,
+	networkName string,
+	cmd []string,
+) (testcontainers.Container, error) {
+	containerName := generatingContainer
+
+	lc := new(MyLogConsumer)
+	lc.Prefix = containerName
+
+	req := testcontainers.ContainerRequest{
+		Image: "ghcr.io/open-telemetry/opentelemetry-collector-contrib/telemetrygen:latest",
+		LogConsumerCfg: &testcontainers.LogConsumerConfig{
+			Consumers: []testcontainers.LogConsumer{lc},
+		},
+		Networks: []string{networkName},
+		Name:     containerName,
+		Cmd:      cmd,
+	}
+
+	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+
+	return container, err
+}
+
+func loadResultFile(
+	ctx context.Context,
+	container testcontainers.Container,
+	resultFilePath string,
+) ([]string, error) {
+	r, err := container.CopyFileFromContainer(ctx, resultFilePath)
+	if err != nil {
+		return make([]string, 0), err
+	}
+
+	content, err := io.ReadAll(r)
+	if err != nil {
+		return make([]string, 0), err
+	}
+
+	log.Print("*** raw content:\n" + string(content) + "\n")
+	lines := strings.Split(string(content), "\n")
+	return lines, nil
 }
 
 type MyLogConsumer struct {
@@ -136,7 +343,5 @@ type MyLogConsumer struct {
 }
 
 func (lc *MyLogConsumer) Accept(l testcontainers.Log) {
-	msg := fmt.Sprintf("***%s: %s", lc.Prefix, string(l.Content))
-	//fmt.Print(msg)
-	log.Print(msg)
+	log.Printf("***%s: %s", lc.Prefix, string(l.Content))
 }
