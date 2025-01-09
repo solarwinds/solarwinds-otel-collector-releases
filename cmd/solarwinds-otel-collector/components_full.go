bf3cce801eb0927fde44e2edce8916c1ad76832f
Marek Simek <marek.simek@solarwinds.com>
2025-01-09 09:23:05 +0100

    feature: Add k8s Distribution

--- cmd/solarwinds-otel-collector/components_full.go
+++ cmd/solarwinds-otel-collector/components_full.go
@@ -1,4 +1,4 @@
-// Copyright 2024 SolarWinds Worldwide, LLC. All rights reserved.
+// Copyright 2025 SolarWinds Worldwide, LLC. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,15 +12,18 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:build full
+
 package main
 
 import (
 	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/fileexporter"
-	"github.com/solarwinds/solarwinds-otel-collector/exporter/solarwindsexporter"
-	"github.com/solarwinds/solarwinds-otel-collector/extension/solarwindsextension"
 	"go.opentelemetry.io/collector/exporter/debugexporter"
 	"go.opentelemetry.io/collector/exporter/nopexporter"
 	"go.opentelemetry.io/collector/exporter/otlpexporter"
+
+	"github.com/solarwinds/solarwinds-otel-collector/exporter/solarwindsexporter"
+	"github.com/solarwinds/solarwinds-otel-collector/extension/solarwindsextension"
 	// extensions
 	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/ackextension"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/asapauthextension"
---
+++ cmd/solarwinds-otel-collector/components_k8s.go
@@ -0,0 +1,133 @@
+// Copyright 2025 SolarWinds Worldwide, LLC. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:build k8s
+
+package main
+
+import (
+	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/fileexporter"
+	"go.opentelemetry.io/collector/exporter/debugexporter"
+	"go.opentelemetry.io/collector/exporter/nopexporter"
+	"go.opentelemetry.io/collector/exporter/otlpexporter"
+
+	"github.com/solarwinds/solarwinds-otel-collector/exporter/solarwindsexporter"
+	"github.com/solarwinds/solarwinds-otel-collector/extension/solarwindsextension"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/healthcheckextension"
+	"go.opentelemetry.io/collector/connector"
+	"go.opentelemetry.io/collector/connector/forwardconnector"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/filelogreceiver"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/k8seventsreceiver"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/k8sobjectsreceiver"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/prometheusreceiver"
+	"go.opentelemetry.io/collector/receiver/otlpreceiver"
+
+	// processors
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/attributesprocessor"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/cumulativetodeltaprocessor"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/deltatocumulativeprocessor"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/deltatorateprocessor"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/filterprocessor"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/groupbyattrsprocessor"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/groupbytraceprocessor"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/k8sattributesprocessor"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/metricsgenerationprocessor"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/metricstransformprocessor"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/resourcedetectionprocessor"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/resourceprocessor"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/transformprocessor"
+	"go.opentelemetry.io/collector/processor/memorylimiterprocessor"
+
+	"go.opentelemetry.io/collector/exporter"
+	"go.opentelemetry.io/collector/extension"
+	"go.opentelemetry.io/collector/otelcol"
+	"go.opentelemetry.io/collector/processor"
+	"go.opentelemetry.io/collector/receiver"
+)
+
+func components() (otelcol.Factories, error) {
+	var err error
+	factories := otelcol.Factories{}
+
+	factories.Extensions, err = extension.MakeFactoryMap(
+		solarwindsextension.NewFactory(),
+		healthcheckextension.NewFactory(),
+		filestorageextension.NewFactory(),
+		k8sobjectsextension.NewFactory(),
+	)
+
+	if err != nil {
+		return otelcol.Factories{}, err
+	}
+
+	factories.Receivers, err = receiver.MakeFactoryMap(
+		prometheusreceiver.NewFactory(),
+		k8seventsreceiver.NewFactory(),
+		k8sobjectsreceiver.NewFactory(),
+		filelogreceiver.NewFactory(),
+		journalreceiver.NewFactory(),
+		otlpreceiver.NewFactory(),
+		recievercreator.NewFactory(),
+		simplereceiver.NewFactory(),
+	)
+
+	if err != nil {
+		return otelcol.Factories{}, err
+	}
+
+	factories.Processors, err = processor.MakeFactoryMap(
+		attributesprocessor.NewFactory(),
+		cumulativetodeltaprocessor.NewFactory(),
+		deltatocumulativeprocessor.NewFactory(),
+		deltatorateprocessor.NewFactory(),
+		filterprocessor.NewFactory(),
+		groupbyattrsprocessor.NewFactory(),
+		groupbytraceprocessor.NewFactory(),
+		k8sattributesprocessor.NewFactory(),
+		metricsgenerationprocessor.NewFactory(),
+		metricstransformprocessor.NewFactory(),
+		resourcedetectionprocessor.NewFactory(),
+		resourceprocessor.NewFactory(),
+		transformprocessor.NewFactory(),
+		memorylimiterprocessor.NewFactory(),
+	)
+
+	if err != nil {
+		return otelcol.Factories{}, err
+	}
+
+	factories.Exporters, err = exporter.MakeFactoryMap(
+		fileexporter.NewFactory(),
+		debugexporter.NewFactory(),
+		nopexporter.NewFactory(),
+		otlpexporter.NewFactory(),
+		solarwindsexporter.NewFactory(),
+	)
+
+	if err != nil {
+		return otelcol.Factories{}, err
+	}
+
+	factories.Connectors, err = connector.MakeFactoryMap(
+		forwardconnector.NewFactory(),
+	)
+
+	if err != nil {
+		return otelcol.Factories{}, err
+	}
+
+	return factories, nil
+}
