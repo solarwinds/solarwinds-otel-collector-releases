cae1772d8c3251ffeedf7ad001d5fa5ce1983514
Oleksandr Turytsia <italexx.cz@gmail.com>
2025-03-20 09:17:34 +0100

    chore: simplify rege

--- receiver/swok8sobjectsreceiver/receiver.go
+++ receiver/swok8sobjectsreceiver/receiver.go
@@ -404,7 +404,7 @@ func getObjectHashes(udata *unstructured.Unstructured) (*objecthashes, error) {
 		return nil, err
 	}
 
-	timestampRegex := regexp.MustCompile(`/(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z))/`)
+	timestampRegex := regexp.MustCompile(`\b\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d+)?(Z|[+-]\d{2}:\d{2})\b?`)
 
 	// do not report changes in any timestamps
 	cleanedBytes := timestampRegex.ReplaceAll(udataBytes, []byte(""))
