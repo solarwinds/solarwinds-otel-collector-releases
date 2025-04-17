8522005c21322f577c35378f1f4a856cd08f05ef
janatrelakova <jana.trelakova@solarwinds.com>
2025-04-17 14:58:07 +0200

    Add licenses

--- connector/solarwindsentityconnector/connector_test.go
+++ connector/solarwindsentityconnector/connector_test.go
@@ -12,28 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package internal
-
-import (
-	"github.com/stretchr/testify/assert"
-	"testing"
-)
-
-var (
-	expectedEntities = map[string]Entity{
-		"testEntityType":  NewEntity("testEntityType", []string{"id1", "id2"}, []string{"attr1", "attr2"}),
-		"testEntityType2": NewEntity("testEntityType2", []string{"id3"}, []string{"attr3"}),
-	}
-)
-
-func TestGetEntity(t *testing.T) {
-	// arrange
-	entities := NewEntities(expectedEntities)
-
-	// act
-	actualEntity := entities.GetEntity("testEntityType")
-
-	// assert
-	assert.NotNil(t, actualEntity)
-	assert.Equal(t, expectedEntities["testEntityType"], actualEntity)
-}
+package solarwindsentityconnector
