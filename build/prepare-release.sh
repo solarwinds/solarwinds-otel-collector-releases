ba5d4f006e7c85147fc68c7592b9dce6622eb099
akumetsu183 <milan.kuba@solarwinds.com>
2025-05-20 14:43:13 +0200

    improved prepare-release.sh

--- build/prepare-release.sh
+++ build/prepare-release.sh
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-if [ "$#" -ne 3 ]; then
+if [ "$#" -lt 1 ]; then
     echo "Usage: $0 <version> $1 <swi_contrib_version> $2 <builder_version>"
     exit 1
 fi
@@ -35,21 +35,34 @@ else
     echo "CHANGELOG.md already contains 'v$VERSION', no update made."
 fi
 
-# Update release manifest files
-ALL_MANIFEST_YAML=$(find . -name "manifest.yaml" -type f | sort)
-for f in $ALL_MANIFEST_YAML; do
-    # update solarwinds contrib references
-    perl -pi -e "s|^(\s+- gomod: github.com/solarwinds/solarwinds-otel-collector-contrib/[^ ]*) v[0-9]+\.[0-9]+\.[0-9]+$|\1 v$CONTRIB_VERSION|" "$f"
-    echo "References to 'github.com/solarwinds/solarwinds-otel-collector-contrib' in $f updated with version v$CONTRIB_VERSION"
-done
+if [ -z "$CONTRIB_VERSION" ]; then
+  echo "CONTRIB_VERSION not set, skipping."
+else
+  # Update release manifest files
+  ALL_MANIFEST_YAML=$(find . -name "manifest.yaml" -type f | sort)
+  for f in $ALL_MANIFEST_YAML; do
+      # update solarwinds contrib references
+      perl -pi -e "s|^(\s+- gomod: github.com/solarwinds/solarwinds-otel-collector-contrib/[^ ]*) v[0-9]+\.[0-9]+\.[0-9]+$|\1 v$CONTRIB_VERSION|" "$f"
+      echo "References to 'github.com/solarwinds/solarwinds-otel-collector-contrib' in $f updated with version v$CONTRIB_VERSION"
+  done
 
-# We need to run go mod tidy after raising versions of solarwinds-otel-collector-contrib components
-echo "Running go mod tidy"
-find . -name "go.mod" -execdir sh -c 'go mod tidy' \;
+  # We need to run go mod tidy after raising versions of solarwinds-otel-collector-contrib components
+  echo "Running go mod tidy"
+  find . -name "go.mod" -execdir sh -c 'go mod tidy' \;
+fi
 
-# update BUILDER_VERSION env var in ./.github/workflows/buildAndTest.yml
-perl -pi -e "s|BUILDER_VERSION: \"v[0-9]+\.[0-9]+\.[0-9]+\"|BUILDER_VERSION: \"v$BUILDER_VERSION\"|g" ./.github/workflows/buildAndTest.yml
-echo "Updated BUILDER_VERSION in /.github/workflows/buildAndTest.yaml to version $BUILDER_VERSION"
+if [ -z "$BUILDER_VERSION" ]; then
+  echo "BUILDER_VERSION not set, skipping."
+else
+  YAML_FILE="./.github/workflows/buildAndTest.yml"
+  if [ ! -f "$YAML_FILE" ]; then
+      echo "buildAndTest.yml not found!"
+      exit 1
+  fi
+  # update BUILDER_VERSION env var in ./.github/workflows/buildAndTest.yml
+  perl -pi -e "s|BUILDER_VERSION: \"v[0-9]+\.[0-9]+\.[0-9]+\"|BUILDER_VERSION: \"v$BUILDER_VERSION\"|g" "$YAML_FILE"
+  echo "Updated BUILDER_VERSION in /.github/workflows/buildAndTest.yaml to version $BUILDER_VERSION"
+fi
 
 # update pkg\version\version.go to set the actual release version
 GO_VERSION_FILE="./pkg/version/version.go"
