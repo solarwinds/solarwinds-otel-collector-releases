c450c3dc67788cb51fc19f3ebc02c89455e60c68
adam-schreiber_swi <adam.schreiber@solarwinds.com>
2026-06-22 11:21:33 +0200

    chore: sync

--- build/check-licenses.sh
+++ build/check-licenses.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2025 SolarWinds Worldwide, LLC. All rights reserved.
+# Copyright 2026 SolarWinds Worldwide, LLC. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
